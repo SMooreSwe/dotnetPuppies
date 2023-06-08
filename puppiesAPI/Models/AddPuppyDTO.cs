161cae83b3aac7e28c180e73a536566ebb4f81c6
SMooreSwe <stevedm88@gmail.com>
2023-06-08 15:29:51 +0200

    JSON serialised

--- puppiesAPI/Models/AddPuppyDTO.cs
+++ puppiesAPI/Models/AddPuppyDTO.cs
@@ -1,12 +1,16 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 public class AddPuppyDTO
 {
     public string Id { get; set; } = Guid.NewGuid().ToString();
     [Required]
+    [JsonPropertyName("name")]
     public string? Name { get; set; }
     [Required]
+    [JsonPropertyName("birthdate")]
     public string? BirthDate { get; set; }
     [Required]
+    [JsonPropertyName("breed")]
     public string? Breed { get; set; }
 }
--- puppiesAPI/Models/Puppy.cs
+++ puppiesAPI/Models/Puppy.cs
@@ -1,7 +1,16 @@
+using System.Text.Json.Serialization;
+
 public class Puppy
 {
-    public string Id { get; set; }
+    [JsonPropertyName("_id")]
+    public string Id { get; set; } = "";
+    
+    [JsonPropertyName("name")]
     public string? Name { get; set; }
+
+    [JsonPropertyName("birthdate")]
     public string? BirthDate { get; set; }
+    
+    [JsonPropertyName("breed")]
     public string? Breed { get; set; }
 }
